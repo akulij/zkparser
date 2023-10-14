50b4cba280e33b3ae885ff1e3d708fe9b772ffb7
Akulij <aakulij@gmail.com>
2023-10-14 16:21:06 +0300

    add more tests

--- tests/test_zkwallet.py
+++ tests/test_zkwallet.py
@@ -28,3 +28,17 @@ async def test_wallet_linea():
     pro_code = "elemgmU13"
     eraw = await get_wallet_linea_mainnet(wallet, pro_code)
     print(eraw)
+
+@mark.asyncio
+async def test_wallet_scrool():
+    wallet = "0xd83690Cb555FCA66FD572f9b7A27dE79bF5D9B06"
+    pro_code = "elemgmU13"
+    info = await get_wallet_scroll(wallet, pro_code)
+    print(info)
+
+@mark.asyncio
+async def test_wallet_zero():
+    wallet = "0xa3f7FE5f82eA5DB2e8F163AfB99494b3f0c278a6"
+    pro_code = "elemgmU13"
+    info = await get_wallet_zero(wallet, pro_code)
+    print(info)
